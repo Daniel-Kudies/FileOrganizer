7a4cbce5fb492cf4f34926098672538a9febd051
Daniel Kudies <daniel.kudies@googlemail.com>
2024-10-28 16:41:08 +0100

    solved some pylint errors

--- file_organizer/file_sorter.py
+++ file_organizer/file_sorter.py
@@ -1,9 +1,17 @@
+"""
+FileSorter module for organizing files by date and extensions.
+
+This module contains the FileSorter class, which provides methods to
+sort files in specified directories based on configuration settings
+such as sorting by date or extension.
+"""
 import os
 import shutil
 import logging
 import datetime
 from json_loader import JsonLoader
 
+"""Class to handle file sorting based on configurations loaded from JSON."""
 class FileSorter:
     def __init__ (self):
         json_loader = JsonLoader()
@@ -20,9 +28,9 @@ class FileSorter:
                 path = os.path.expanduser(details.get("path", ""))
                 sortingoptions = details.get("sorting", {})
 
-                if (sortingoptions.get("by_date")):
+                if sortingoptions.get("by_date"):
                     self.sort_file_date(path)
-                if (sortingoptions.get("by_extension")):
+                if sortingoptions.get("by_extension"):
                     self.sort_file_extensions(path, sortingoptions.get("by_date"))
 
         except FileNotFoundError as e:
@@ -30,40 +38,40 @@ class FileSorter:
         except PermissionError as e:
             logging.error(f"PermissionError: {e}")
         except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")  
+            logging.error(f"An unexpected error occurred: {e}")
 
     def sort_file_extensions(self, path, bydate):
         try:
             logging.debug(f"Sorting by File Extensions in Directory: {path}")
 
             self.create_extensions_folders(path)
-            if (bydate):
+            if bydate:
                 logging.info("Sorting by extensions within date folders.")
 
                 for year_folder in os.scandir(path):
                     if year_folder.is_dir():
                         year_path = os.path.join(path, year_folder.name)
                         self.create_extensions_folders(year_path)
-                    
+
                         for file in os.scandir(year_path):
                             if file.is_file():
                                 extension = os.path.splitext(file.name)[1]
                                 for category, extensions in self.file_extensions.items():
                                     if extension in extensions:
-                                        target_folder = os.path.join(year_path, category)
-                                        os.makedirs(target_folder, exist_ok=True)
-                                        shutil.move(file.path, os.path.join(target_folder, file.name))
-                                        logging.info(f"Moved file {file.name} to {target_folder}")
+                                        targetfolder = os.path.join(year_path, category)
+                                        os.makedirs(targetfolder, exist_ok=True)
+                                        shutil.move(file.path, os.path.join(targetfolder, file.name))
+                                        logging.info(f"Moved file {file.name} to {targetfolder}")
 
             for file in os.scandir(path):
                 if file.is_file():
                     extension = os.path.splitext(file.name)[1]
                     for category, extensions in self.file_extensions.items():
                         if extension in extensions:
-                            target_folder = os.path.join(path, category)
-                            os.makedirs(target_folder, exist_ok=True)
-                            shutil.move(file.path, os.path.join(target_folder, file.name))
-                            logging.info(f"Moved file {file.name} to {target_folder}")
+                            targetfolder = os.path.join(path, category)
+                            os.makedirs(targetfolder, exist_ok=True)
+                            shutil.move(file.path, os.path.join(targetfolder, file.name))
+                            logging.info(f"Moved file {file.name} to {targetfolder}")
 
 
         except FileNotFoundError as e:
@@ -72,11 +80,11 @@ class FileSorter:
             logging.error(f"Permission denied: {e}")
         except Exception as e:
             logging.error(f"An unexpected error occurred: {e}")
-    
+
     def sort_file_date(self, path):
         try:
             logging.debug(f"Sorting by File Date in Directory: {path}")
-            
+ 
             self.create_date_folder(path)
 
             for file in os.scandir(path):
@@ -92,7 +100,7 @@ class FileSorter:
             logging.error(f"Permission denied: {e}")
         except Exception as e:
             logging.error(f"An unexpected error occurred: {e}")
-        
+
     def create_date_folder(self, path):
         try:
             logging.debug(f"Looking for Folder in Directory: {path} if they exists do nothing else create it")
@@ -115,7 +123,7 @@ class FileSorter:
             logging.error(f"PermissionError while creating date folders in {path}: {e}")
         except Exception as e:
             logging.error(f"An unexpected error occurred while creating date folders in {path}: {e}")
-    
+
     def create_extensions_folders(self, path):
         try:
             logging.debug(f"Creating Extension Folders in Directory: {path} if they exists do nothing else create it")
@@ -130,8 +138,8 @@ class FileSorter:
                             existing_extensions.add(category)
 
             for category in existing_extensions:
-                target_folder = os.path.join(path, category)
-                os.makedirs(target_folder, exist_ok=True)
+                targetfolder = os.path.join(path, category)
+                os.makedirs(targetfolder, exist_ok=True)
                 logging.info(f"Created folder for category: {category} in {path}")
 
         except FileNotFoundError as e:
