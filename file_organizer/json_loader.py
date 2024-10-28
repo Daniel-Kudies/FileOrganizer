23bb66aa69837046b58043cb94efbe1d8f3eb217
Daniel Kudies <daniel.kudies@googlemail.com>
2024-10-28 18:08:08 +0100

    fixed pylint for json_loader

--- file_organizer/json_loader.py
+++ file_organizer/json_loader.py
@@ -32,10 +32,10 @@ class JsonLoader:
             file_path (str): The path to the JSON file to load.
 
         Returns:
-            dict: The loaded JSON data if successful, or None if an error occurred.
+            dict or None: The loaded JSON data if successful, or None if an error occurred.
         """
         try:
-            with open(file_path, "r") as x:
+            with open(file_path, "r", encoding="utf-8") as x:
                 data = json.load(x)
                 logging.info("Data from %s loaded successfully.", file_path)
                 return data
@@ -44,15 +44,37 @@ class JsonLoader:
             logging.error("Configuration file %s not found.", file_path)
         except json.JSONDecodeError:
             logging.error("Error decoding JSON from %s.", file_path)
+        return None
 
     def load_config(self):
+        """Loads the main configuration from the specified config file.
+
+        This method uses the load_json method to read the main
+        configuration file and store the data in the config attribute.
+        """
         self.config = self.load_json(self.config_file)
 
     def load_file_extensions(self):
+        """Loads file extensions mapping from the specified file.
+
+        This method uses the load_json method to read the file
+        extensions configuration file and store the data in the
+        file_extensions attribute.
+        """
         self.file_extensions = self.load_json(self.file_extensions_config)
 
     def get_config(self):
+        """Retrieves the loaded configuration data.
+
+        Returns:
+            dict: The loaded configuration data.
+        """
         return self.config
 
     def get_file_extensions(self):
+        """Retrieves the loaded file extensions data.
+
+        Returns:
+            dict: The loaded file extensions data.
+        """
         return self.file_extensions
